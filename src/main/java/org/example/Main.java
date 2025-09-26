31821aebfc62936e49db369a9545f35e32619db4
selcuk-ulgul <sulgul1111@gmail.com>
2025-09-26 18:32:39 +0300

    anasayfa düzenlendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        Person person=new Person("Jobh","Doe",20);
+        Person person1=new Person("Jobh","Doe",20,"Canada");
+        Person person2=new Person("Jobh","Doe",20,"Canada","Tennis","Dog");
+        System.out.println(person.firstname+"-"+person.lastname+"-"+person.age);
         System.out.println("Hello world");
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class Person {
+    String firstname;
+    String lastname;
+    int age;
+    String country;
+    String hobby;
+    String favouritePet;
+
+   public Person(String firstname,
+           String lastname,
+           int age) {
+
+        this.age = age;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Person(String firstname,
+           String lastname,
+           int age, String country) {
+
+        this(firstname, lastname, age);
+        this.country = country;
+    }
+
+    public Person(String firstname,
+           String lastname,
+           int age, String country, String hobby,
+           String favouritePet) {
+
+        this(firstname, lastname, age, country);
+        this.hobby = hobby;
+        this.favouritePet = favouritePet;
+    }
+
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        boolean sonuc = false;
+        if (age < 19 && age > 13) sonuc = true;
+        return sonuc;
+    }
+}
